0036d1db2ec6a6174e0675a85f28e452d3463a24
komekez <kausr2595@gmail.com>
2022-06-25 13:58:08 -0700

    Engine Construction Complete

---
+++ src/engine.py
@@ -0,0 +1,75 @@
+from tqdm import tqdm
+import torch
+
+
+"""
+bceLoss : Reconstruction Loss
+mu : Mean
+logVar : Log Variance
+"""
+def lossFunction(bceLoss, mu, logVar):
+
+    #Reconstructional Loss
+    BCE = bceLoss
+
+    #KL Divergence Loss
+    KLD = -0.5+torch.sum(1+logVar - mu.pow(2) - logVar.exp())
+
+    return BCE+KLD
+
+"""
+bceLoss : Reconstruction Loss
+mu : Mean
+logVar : Log Variance
+"""
+def train(model, dataloader, dataset, optimizer, criterion):
+    model.train()
+    runningLoss = 0.0
+    counter = 0
+
+    for i,data in tqdm(enumerate(dataloader), total=int(len(dataset)/dataloader.batchSize)):
+        counter += 1
+        data = data[0]
+        optimizer.zero_grad()
+
+        reconstruction, mu, logVar = model(data)
+        bceLoss = criterion(reconstruction, data)
+
+        loss = lossFunction(bceLoss, mu, logVar)
+        loss.backward()
+
+        runningLoss += loss.item()
+
+        optimizer.step()
+
+    trainLoss = runningLoss/counter
+    return trainLoss
+
+"""
+bceLoss : Reconstruction Loss
+mu : Mean
+logVar : Log Variance
+"""
+def validate(model, dataloader, criterion):
+    model.eval()
+    runningLoss = 0.0
+
+    counter = 0
+
+    with torch.no_grad():
+        for i, data in tqdm(enumerate(dataloader), total=int(len(dataset)/dataloader.batchSize)):
+            counter += 1
+            data = data[0]
+
+            reconstruction, mu, logVar = model(data)
+            bceLoss = criterion(reconstruction, data)
+
+            loss = lossFunction(bceLoss, mu, logVar)
+            runningLoss += loss.item()
+
+            if(i == int(len(dataset)/dataloader.batchSize)-1):
+                reconImage = reconstruction
+    valLoss = runningLoss/counter
+    return valLoss, reconImage
+
+
